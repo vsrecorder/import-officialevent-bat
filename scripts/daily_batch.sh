9237788adc27a35aebc8bf2eb63816b87b704fdc
sosomasox <sosomasox@gmail.com>
2025-01-04 23:17:57 +0900

    update

--- scripts/daily_batch.sh
+++ scripts/daily_batch.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-source .env
+source /home/ubuntu/import-officialevent-bat/.env
 
 curl -X POST -H 'Content-type: application/json' --data '{"text":"日次バッチを開始します。"}' ${SLACK_WEBHOOK_URL}
-
 go run cmd/daily_batch/main.go 2> /dev/null
-
 curl -X POST -H 'Content-type: application/json' --data '{"text":"日次バッチが終了しました。"}' ${SLACK_WEBHOOK_URL}
 
 exit 0
